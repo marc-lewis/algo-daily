7c0da37d7f4a5247128ee12ec8c1f0c885b051fc
Marc Lewis <marc@marclewis.io>
2020-09-16 09:20:40 +0100

    add a new test

--- algos/reverse-string/ReverseString.test.ts
+++ algos/reverse-string/ReverseString.test.ts
@@ -3,10 +3,14 @@ import reverseString from './ReverseString'
 describe('ReverseString', () => {
     it('should reverse `a`', () => {
         const str = 'a'
-        expect(reverseString()).toBe('a')
+        expect(reverseString(str)).toBe('a')
     })
     it('should reverse `b`', () => {
         const str = 'b'
-        expect(reverseString()).toBe('b')
+        expect(reverseString(str)).toBe('b')
+    })
+    it('should reverse `ab`', () => {
+        const str = 'ab'
+        expect(reverseString(str)).toBe('ba')
     })
 })
