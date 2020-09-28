9d99ac3f3376904d575b6acdd2c0a2343aca8977
Marc Lewis <marc@marclewis.io>
2020-09-28 17:33:45 +0100

    Add n/2 test

--- algos/reverse-string/ReverseString.ts
+++ algos/reverse-string/ReverseString.ts
@@ -1,7 +1,28 @@
+/**
+ * A loop of O(N)
+ * @param str The string to reverse
+ */
+// export default function (str: string): string {
+//     let reversedString = ''
+//     for (let i = str.length; i > 0; i--) {
+//         reversedString += str[i - 1]
+//     }
+//     return reversedString
+// }
+
+
 export default function (str: string): string {
-    let reversedString = ''
-    for (let i = str.length; i > 0; i--) {
-        reversedString += str[i - 1]
+    let strArray = str.split('')
+    let end = str.length - 1
+
+    for(let i = 0; i < end; i++) {
+        let temp = strArray[i]
+        strArray[i] = strArray[end]
+        strArray[end] = temp
+        end--
     }
-    return reversedString
+
+    return strArray.join('')
 }
+
+
